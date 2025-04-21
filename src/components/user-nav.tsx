cda66edb5508554927917a53cfe15f24b0c6a4a3
ren3459 <55650591+ren3459@users.noreply.github.com>
2025-04-21 12:28:07 +0300

    The username from keycloak has been done (#28)
    The username from keycloak has been done (#28)

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
-// import { signOut } from 'next-auth/react';
-// TODO: import { signOut, useSession } from 'next-auth/react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAuth } from "react-oidc-context";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -14,36 +13,25 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-/*
- * TODO:
- *  1.add signOut actions
-   <DropdownMenuItem onClick={() => signOut()}>
-    <LogOut />
-    Log out
-  </DropdownMenuItem>
- * 2. Uncomment const { data: session } = useSession();
- */
 export function UserNav() {
-  // const { data: session } = useSession();
-  const { data: session } = { data: { user: { image: null, name: "a5gurnakov", email: null } } };
   const navigate = useNavigate();
+  const auth = useAuth();
 
-  if (session) {
+  if (auth?.user?.profile) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              <AvatarImage src={session.user?.image ?? ""} alt={session.user?.name ?? ""} />
-              <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+              <AvatarFallback>{auth?.user?.profile.name?.[0]}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">{session.user?.name}</p>
-              <p className="text-xs leading-none text-muted-foreground">{session.user?.email}</p>
+              <p className="text-sm font-medium leading-none">{auth?.user?.profile.name}</p>
+              <p className="text-xs leading-none text-muted-foreground">{auth?.user?.profile?.email}</p>
             </div>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
