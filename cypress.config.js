7d002dc8700c6fb798d64663b03c91169401f283
a5gurnakov <a5gurnakov@gmail.com>
2025-04-23 12:56:26 +0300

    The cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     excludeSpecPattern: [
       '**/2-advanced-examples/**'
