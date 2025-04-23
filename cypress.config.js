9f793bdecefd2ab137ec28ebacf5a7190696add5
a5gurnakov <a5gurnakov@gmail.com>
2025-04-23 12:41:01 +0300

    The draft cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    excludeSpecPattern: [
+      '**/2-advanced-examples/**'
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
