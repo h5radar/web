078f2c40ac4e8654a101fcb0f97839934a2d4dd4
a5gurnakov <a5gurnakov@gmail.com>
2025-05-16 11:06:50 +0300

    The tddocs has been added

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
-import stylisticTs from "@stylistic/eslint-plugin-ts";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import tsdoceslint from "eslint-plugin-tsdoc"
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
 
 export default tseslint.config(
   { ignores: ["coverage", "dist", "src/components/ui"] },
@@ -18,12 +19,14 @@ export default tseslint.config(
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@stylistic/ts": stylisticTs,
+      "tsdoc": tsdoceslint
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@stylistic/ts/indent": ["error", 2],
       "@stylistic/ts/quotes": ["error", "double"],
+      "tsdoc/syntax": "error",
     },
   },
 );
