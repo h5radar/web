28b99dd2b45034107fb46f91632e8086f67c9f48
a5gurnakov <a5gurnakov@gmail.com>
2025-05-22 15:17:32 +0300

    The query params moved to lib

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -1,3 +1,9 @@
+/**
+ * Create query params.
+ *
+ * @param params - input values: page, size per page, sort (["title", "asc"])
+ * @returns params for URL
+ */
 export function createQueryParams(params: Record<string, string | number | boolean | string[]>) {
   const result = new URLSearchParams();
   Object.entries(params).forEach(([key, value]) => {
