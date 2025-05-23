7cdf7fdfaacf44db900c6b28c8d00bd02f172b96
a5gurnakov <a5gurnakov@gmail.com>
2025-05-23 14:46:32 +0300

    The keys have been moved to constants

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -6,8 +6,8 @@ export const GET_AUTH_HEALTH = "Get auth health";
 /*
  * TanStack query keys for technology
  */
-export const GET_TECHNOLOGIES = "Get technologies";
-export const GET_TECHNOLOGY = "Get technology";
 export const CREATE_TECHNOLOGY = "Create technology";
 export const UPDATE_TECHNOLOGY = "Update technology";
 export const DELETE_TECHNOLOGY = "Delete technology";
+export const GET_TECHNOLOGY = "Get technology";
+export const GET_TECHNOLOGIES = "Get technologies";
