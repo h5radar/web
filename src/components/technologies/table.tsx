ab8642ec560d0e0e9af96f9169beb5ffd3836377
ren3459 <55650591+ren3459@users.noreply.github.com>
2025-05-26 21:12:55 +0300

    hiding a column depending on the width

--- src/components/technologies/table.tsx
+++ src/components/technologies/table.tsx
@@ -142,9 +142,6 @@ export const TechnologyTable = ({
     moved: false,
     active: false,
   });
-  console.log("isMobile:", isMobile);
-  console.log("isVisible:", !isMobile);
-  console.log("fuck3:", columnVisibility);
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [pagination, setPagination] = React.useState({
     pageIndex: pageIndex,
@@ -152,6 +149,20 @@ export const TechnologyTable = ({
   });
   const sortableId = React.useId();
   const sensors = useSensors(useSensor(MouseSensor, {}), useSensor(TouchSensor, {}), useSensor(KeyboardSensor, {}));
+
+  // only for log, remove before merge
+  React.useEffect(() => {
+    console.log("compl isMobile:", isMobile);
+    console.log("compl isVisible:", !isMobile);
+    console.log("compl fuck3:", columnVisibility);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [columnVisibility]);
+  //
+
+  React.useEffect(() => {
+    console.log("isMobile only:", isMobile); //
+    setColumnVisibility((prev) => ({ ...prev, website: !isMobile }));
+  }, [isMobile]);
 
   const dataIds = React.useMemo<UniqueIdentifier[]>(() => data?.map(({ id }) => id) || [], [data]);
 
