2cf6b10d75d8a05d179b847db57c58bded8577a9
arufanov <arufanov@gmail.com>
2025-05-29 17:40:21 +0300

    The user has been added

--- tests/pages/account.test.tsx
+++ tests/pages/account.test.tsx
@@ -12,24 +12,6 @@ const queryClient = new QueryClient({
   },
 });
 
-vi.mock("react-oidc-context", () => ({
-  useAuth() {
-    const { isLoading, isAuthenticated } = {
-      isLoading: false,
-      isAuthenticated: true,
-    };
-    return {
-      isLoading,
-      isAuthenticated,
-      signinRedirect: vi.fn(),
-      removeUser: vi.fn(),
-      settings: {},
-      user: { profile: { sub: "5483cbe8-15cd-4bf5-911e-6ca455e533c9", preferred_username: "alice" } },
-    };
-  },
-  hasAuthParams: vi.fn(() => true),
-}));
-
 describe("AccountPage", () => {
   afterEach(() => {
     vi.restoreAllMocks();
--- vitest.config.mts
+++ vitest.config.mts
@@ -12,7 +12,7 @@ export default defineConfig({
     environment: "jsdom",
     outputFile: { junit: "./tests/results/junit.xml", html: "./tests/results/index.html" },
     reporters: ["default", "junit", "html"],
-    setupFiles: ["./tests/setup.mts","./tests/mocks.ts",],
+    setupFiles: ["./tests/setup.ts","./tests/mocks.ts",],
     coverage: {
       include: ["src/**"],
       exclude: ["src/ui/**"],
