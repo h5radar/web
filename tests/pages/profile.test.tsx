efff2968eb117e67170e9cce920eb6d7ad2d8b33
arufanov <arufanov@gmail.com>
2025-05-29 17:46:12 +0300

    The user has been added

--- tests/pages/profile.test.tsx
+++ tests/pages/profile.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it} from "vitest";
+import { describe, expect, it } from "vitest";
 
 import ProfilePage from "@/pages/profile";
 
