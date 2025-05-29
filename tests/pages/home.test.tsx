ba4b906e912be26245f95c1c493d8569d90737b0
arufanov <arufanov@gmail.com>
2025-05-29 16:57:33 +0300

    The user has been added

--- tests/pages/home.test.tsx
+++ tests/pages/home.test.tsx
@@ -1,9 +1,51 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { AuthProvider } from "react-oidc-context";
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+import { userManager } from "@/auth-config";
 
 import HomePage from "@/pages/home";
 
-test("HomePage", () => {
-  render(<HomePage />);
-  expect(screen.getByText("Home")).toBeDefined();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 15,
+    },
+  },
+});
+
+vi.mock("react-oidc-context", () => ({
+  useAuth() {
+    const { isLoading, isAuthenticated } = {
+      isLoading: false,
+      isAuthenticated: true,
+    }
+    return {
+      isLoading,
+      isAuthenticated,
+      signinRedirect: vi.fn(),
+      removeUser: vi.fn(),
+      settings: {},
+      user: { profile: { sub: "5483cbe8-15cd-4bf5-911e-6ca455e533c9", preferred_username: "alice"}},
+    }
+  },
+  hasAuthParams: vi.fn(() => true),
+}))
+
+describe("HomePage", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("Show home page", () => {
+    render(
+      <AuthProvider userManager={userManager}>
+        <QueryClientProvider client={queryClient}>
+          <HomePage />
+        </QueryClientProvider>
+      </AuthProvider>
+    );
+    expect(screen.getByText("Home")).toBeDefined();
+  });
 });
