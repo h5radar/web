62678b27f5bbf91e3f7f8b5e8b7b7fb496f37d2e
arufanov <arufanov@gmail.com>
2025-05-29 17:48:05 +0300

    The user has been added

--- tests/pages/account.test.tsx
+++ tests/pages/account.test.tsx
@@ -1,23 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import AccountPage from "@/pages/account";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 15,
-    },
-  },
-});
-
 describe("AccountPage", () => {
   it("show account page", () => {
     render(
-      <QueryClientProvider client={queryClient}>
-        <AccountPage />
-      </QueryClientProvider>,
+      <AccountPage />
     );
     expect(screen.getByText("Account")).toBeDefined();
   });
--- tests/pages/profile.test.tsx
+++ tests/pages/profile.test.tsx
@@ -1,23 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 import ProfilePage from "@/pages/profile";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 15,
-    },
-  },
-});
-
 describe("ProfilePage", () => {
   it("show profile page", () => {
     render(
-      <QueryClientProvider client={queryClient}>
-        <ProfilePage />
-      </QueryClientProvider>,
+      <ProfilePage />
     );
     expect(screen.getByText("Profile")).toBeDefined();
   });
