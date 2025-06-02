1f6ea7accadf1cfa06b5a2cf4cc2167a20efb21f
arufanov <arufanov@gmail.com>
2025-06-02 15:08:28 +0300

    The code have been refactored for handle function

--- src/pages/technologies/edit.tsx
+++ src/pages/technologies/edit.tsx
@@ -1,6 +1,5 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "react-oidc-context";
-import { toast } from "sonner";
 import { z } from "zod";
 
 import { technologySchema } from "@/schemas/technology";
@@ -15,21 +14,24 @@ export default function EditTechnologyPage() {
   const url = new URL(window.location.href);
   const id = url.pathname.split("/")[3];
 
-  const { data: technology, isFetching: isFetching, isError: isError, error: error } = useGetTechnology(auth, id);
+  const { data: technology, isLoading: isLoading, isError: isError, error: error } = useGetTechnology(auth, id);
   const { mutate: updateTechnology, isPending: isPending } = useUpdateTechnology(auth, queryClient, id);
-
-  if (isError) {
-    toast("Load error", {
-      description: JSON.stringify(error.message),
-    });
-  }
 
   function onSubmit(values: z.infer<typeof technologySchema>) {
     updateTechnology(values);
   }
 
-  if (isFetching) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h1>Error getting technology</h1>
+        <div>{error.message}</div>
+      </>
+    );
   }
 
   return (
