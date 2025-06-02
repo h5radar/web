971d2615aff1f75e2c1289220211f53fb95805af
arufanov <arufanov@gmail.com>
2025-06-02 13:56:49 +0300

    Unused nav item types have been removed (#153)
    Unused nav item types have been removed (#153)

--- src/types/nav-item.ts
+++ src/types/nav-item.ts
@@ -1,3 +1,7 @@
+/**
+ * NavItem is a type for left sidebar
+ *
+ */
 export interface NavItem {
   title: string;
   url: string;
@@ -10,23 +14,3 @@ export interface NavItem {
   isActive?: boolean;
   items?: NavItem[];
 }
-
-export interface NavItemWithChildren extends NavItem {
-  items: NavItemWithChildren[];
-}
-
-export interface NavItemWithOptionalChildren extends NavItem {
-  items?: NavItemWithChildren[];
-}
-
-export interface FooterItem {
-  title: string;
-  items: {
-    title: string;
-    href: string;
-    external?: boolean;
-  }[];
-}
-
-export type MainNavItem = NavItemWithOptionalChildren;
-export type SidebarNavItem = NavItemWithChildren;
