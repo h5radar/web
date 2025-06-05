864c895664b7fac42f76e471cc93fc0eefc3b689
arufanov <arufanov@gmail.com>
2025-06-05 17:05:11 +0300

    The eslint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import js from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y"
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tsdoceslint from "eslint-plugin-tsdoc";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+
+
 
 export default tseslint.config(
   { ignores: ["dist", "src/ui", "tests/coverage"] },
@@ -20,38 +23,46 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       react: react,
+      "jsx-a11y": jsxA11y,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@stylistic": stylistic,
       tsdoc: tsdoceslint,
     },
     rules: {
-      "react/display-name": 2,
-      "react/jsx-key": 2,
-      "react/jsx-no-comment-textnodes": 2,
-      "react/jsx-no-duplicate-props": 2,
-      "react/jsx-no-target-blank": 2,
-      "react/jsx-no-undef": 2,
-      "react/jsx-uses-react": 2,
-      "react/jsx-uses-vars": 2,
-      "react/no-children-prop": 2,
-      "react/no-danger-with-children": 2,
-      "react/no-deprecated": 2,
-      "react/no-direct-mutation-state": 2,
-      "react/no-find-dom-node": 2,
-      "react/no-is-mounted": 2,
-      "react/no-render-return-value": 2,
-      "react/no-string-refs": 2,
-      "react/no-unescaped-entities": 2,
-      "react/no-unknown-property": 2,
-      "react/no-unsafe": 0,
-      // "react/prop-types": 2,
-      "react/require-render-return": 2,
+      "react/display-name": ["error"],
+      "react/jsx-key": ["error"],
+      "react/jsx-no-comment-textnodes": ["error"],
+      "react/jsx-no-duplicate-props": ["error"],
+      "react/jsx-no-target-blank": ["error"],
+      "react/jsx-no-undef": ["error"],
+      "react/jsx-uses-react": ["error"],
+      "react/jsx-uses-vars": ["error"],
+      "react/no-children-prop": ["error"],
+      "react/no-danger-with-children": ["error"],
+      "react/no-deprecated": ["error"],
+      "react/no-direct-mutation-state": ["error"],
+      "react/no-find-dom-node": ["error"],
+      "react/no-is-mounted": ["error"],
+      "react/no-render-return-value": ["error"],
+      "react/no-string-refs": ["error"],
+      "react/no-unescaped-entities": ["error"],
+      "react/no-unknown-property": ["error"],
+      "react/no-unsafe": ["error"],
+      // "react/prop-types": ["error"],
+      "react/require-render-return": ["error"],
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": ["error"],
+      "jsx-a11y/alt-text": ["error"],
+      "jsx-a11y/lang": ["error"],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@stylistic/indent": ["error", 2],
       "@stylistic/quotes": ["error", "double"],
