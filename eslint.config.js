d0dd47539bef1eb019ff0a2679368eb3bb64f8b0
arufanov <arufanov@gmail.com>
2025-06-05 15:56:44 +0300

    The eslint react has been added

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from "@eslint/js";
-import stylistic from "@stylistic/eslint-plugin";
+import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import stylistic from "@stylistic/eslint-plugin";
 import tsdoceslint from "eslint-plugin-tsdoc";
 import globals from "globals";
+
+
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -16,12 +19,14 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      "react": react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       "@stylistic": stylistic,
       tsdoc: tsdoceslint,
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-hooks/exhaustive-deps": ["error"],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
