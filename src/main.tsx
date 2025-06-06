295834158983bcc170b5fe25885b2d8e295b26a6
ren3459 <ren3459@gmail.com>
2025-06-06 18:02:53 +0300

    add error in toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import { AuthProvider } from "react-oidc-context";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { toast } from "sonner";
+import { ZodError } from "zod";
 
 import { onSigninCallback, userManager } from "@/auth-config";
 
@@ -75,9 +76,18 @@ const queryClient = new QueryClient({
   },
   queryCache: new QueryCache({
     onError: (error, query) => {
-      if (query.meta?.errorMessage) {
+      if (!(error instanceof ZodError) && query.meta?.errorMessage) {
         toast.error(query.meta.errorMessage.toString(), {
           description: error.message,
+        });
+      }
+      if (error instanceof ZodError && query.meta?.errorMessage) {
+        toast.error(query.meta.errorMessage.toString(), {
+          description: JSON.parse(error.message).map((item: { message: string; path: string[] }, key: number) => (
+            <div key={key}>
+              in dataset item №{item.path[1]} error: {item.message}
+            </div>
+          )),
         });
       }
     },
