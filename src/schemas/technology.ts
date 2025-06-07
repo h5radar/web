7977e338ec563ae85d31e5f5724517764e89e9a5
arufanov <arufanov@gmail.com>
2025-06-07 18:52:17 +0300

    The tech validation rules have been adjusted with backend

--- src/schemas/technology.ts
+++ src/schemas/technology.ts
@@ -10,7 +10,7 @@ export const technologySchema = z.object({
     .string()
     .min(1, { message: "Description must be at least 3 characters" })
     .max(512, { message: "Description must be less than 512 characters" }),
-  wewebsite: z.string().max(64, { message: "Website must be less than 64 characters" }).optional(),
+  website: z.string().max(64, { message: "Website must be less than 64 characters" }).optional(),
   moved: z
     .number()
     .min(-1, { message: "Moved must be greater or equal than -1" })
