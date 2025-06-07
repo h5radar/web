549c0f48909fb902e142cdd8d039c918612c34d2
arufanov <arufanov@gmail.com>
2025-06-07 17:44:23 +0300

    The tech validation rules have been adjusted with backend

--- src/schemas/technology.ts
+++ src/schemas/technology.ts
@@ -2,20 +2,18 @@ import { z } from "zod";
 
 export const technologySchema = z.object({
   id: z.number(),
-  title: z.string().min(3, {
-    message: "Title must be at least 3 characters",
-  }),
-  description: z.string().min(3, {
-    message: "Description must be at least 3 characters",
-  }),
-  website: z.string().optional(),
+  title: z
+    .string()
+    .min(1, { message: "Title must be at least 3 characters" })
+    .max(64, { message: "Title must be less than 64 characters" }),
+  description: z
+    .string()
+    .min(1, { message: "Description must be at least 3 characters" })
+    .max(512, { message: "Description must be less than 512 characters" }),
+  wewebsite: z.string().max(64, { message: "Website must be less than 64 characters" }).optional(),
   moved: z
     .number()
-    .min(-1, {
-      message: "Moved must be greater or equal than -1",
-    })
-    .max(1, {
-      message: "Moved must be less or equal than 1",
-    }),
+    .min(-1, { message: "Moved must be greater or equal than -1" })
+    .max(1, { message: "Moved must be less or equal than 1" }),
   active: z.boolean(),
 });
