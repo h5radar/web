694141e51420de09aae7601617d314c9c3cfeb63
arufanov <arufanov@gmail.com>
2025-06-11 16:04:19 +0300

    The cmdk fixes

---
+++ tests/components/search-input.test.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { BrowserRouter } from "react-router";
+
+import { SearchProvider } from "@/providers/search";
+import { SearchInput } from "@/components/search-input";
+
+import { accountSearchItem } from "@/constants/search";
+import { billingSearchItem } from "@/constants/search";
+import { notificationSearchItem } from "@/constants/search";
+import { radarSearchItem } from "@/constants/search";
+
+
+describe("SearchInput", () => {
+  it("get search input for empty nav items", () => {
+    render(
+      <BrowserRouter>
+        <SearchProvider navItemList={[]}>
+          <SearchInput />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText("Search")).toBeDefined();
+  });
+
+  it("get search input for account nav items", () => {
+    render(
+      <BrowserRouter>
+        <SearchProvider navItemList={accountSearchItem}>
+          <SearchInput />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText("Search")).toBeDefined();
+  });
+
+  it("get search input for billing nav items", () => {
+    render(
+      <BrowserRouter>
+        <SearchProvider navItemList={billingSearchItem}>
+          <SearchInput />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText("Search")).toBeDefined();
+  });
+
+  it("get search input for billing nav items", () => {
+    render(
+      <BrowserRouter>
+        <SearchProvider navItemList={notificationSearchItem}>
+          <SearchInput />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText("Search")).toBeDefined();
+  });
+
+  it("get search input for billing nav items", () => {
+    render(
+      <BrowserRouter>
+        <SearchProvider navItemList={radarSearchItem}>
+          <SearchInput />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+    expect(screen.getByText("Search")).toBeDefined();
+  });
+
+});
