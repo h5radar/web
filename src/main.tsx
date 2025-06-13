e5b04e213fb16188845b642e0a53c054014e6a6d
ren <ren@ren.ru>
2025-06-13 18:44:02 +0300

    add react-hotkey-hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { HotkeyGlobal } from "./hotkeys/hotkey";
 import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
@@ -89,6 +90,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <AuthProvider userManager={userManager} onSigninCallback={onSigninCallback}>
         <QueryClientProvider client={queryClient}>
+          <HotkeyGlobal />
           <Routes>
             <Route
               path="/"
