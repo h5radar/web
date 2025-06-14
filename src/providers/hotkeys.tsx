1f7ed77acb6919fcab92eea825d2876d56b235ae
arufanov <arufanov@gmail.com>
2025-06-14 16:33:17 +0300

    The hotkeys refactoring

--- src/providers/hotkeys.tsx
+++ src/providers/hotkeys.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import { useAuth } from "react-oidc-context";
 import { useNavigate } from "react-router";
+import { BILLING_ENABLED, NOTIFICATIONS_ENABLED } from "@/constants/application.ts";
 
 interface HotkeysProviderProps {
   children: React.ReactNode;
@@ -16,8 +17,19 @@ export const HotkeysProvider: FC<HotkeysProviderProps> = (props) => {
   useHotkeys("ctrl+shift+u, meta+shift+u", () => auth.signoutRedirect(), [auth]);
   useHotkeys("ctrl+shift+c, meta+shift+c", () => navigate("/account"), [navigate]);
   useHotkeys("ctrl+shift+r, meta+shift+r", () => navigate("/"), [navigate]);
-  useHotkeys("ctrl+shift+l, meta+shift+l", () => navigate("/billing"), [navigate]);
-  useHotkeys("ctrl+shift+o, meta+shift+o", () => navigate("/notifications"), [navigate]);
+
+  useHotkeys("ctrl+shift+o, meta+shift+o", () => {
+    if(NOTIFICATIONS_ENABLED === "true") {
+      navigate("/notifications");
+    }
+  }, [navigate]);
+
+  useHotkeys("ctrl+shift+l, meta+shift+l", () => {
+    if(BILLING_ENABLED === "true") {
+      navigate("/billing");
+    }
+  }, [navigate]);
+
 
   return <>{children}</>;
 };
