cbab36e747328628253f0a76a063e378ecb80883
ren3459 <ren3459@gmail.com>
2025-06-17 23:59:11 +0300

    remove array in response

--- src/types/redux-types.ts
+++ src/types/redux-types.ts
@@ -2,8 +2,4 @@ export interface RadarUser {
   id: number;
   sub: string;
   username: string;
-  licenseDtoList: string[]; // TODO заменить на реальный тип
-  practiceDtoList: string[]; // TODO заменить на реальный тип
-  technologyDtoList: string[]; // TODO заменить на реальный тип
-  productDtoList: string[]; // TODO заменить на реальный тип
 }
