1745193ebbf5fe441e093503bbd932de0feb9252
ren3459 <ren3459@gmail.com>
2025-07-07 22:35:07 +0300

    Show menu item based on url

--- src/components/radar-sidebar.tsx
+++ src/components/radar-sidebar.tsx
@@ -18,8 +18,22 @@ import {
 
 import { radarNavItems } from "@/constants/sidebar";
 
+import { NavItem } from "@/types/nav-item";
+
 import AppSidebarFooter from "@/components/app-sidebar-footer";
 import AppSidebarHeader from "@/components/app-sidebar-header";
+
+function checkIsActive(href: string, item: NavItem, mainNav = false) {
+  return (
+    href === item.url ||
+    href.split("?")[0] === item.url ||
+    !!item?.items?.filter((i) => i.url === href).length ||
+    (href.split("/")[1] !== "" && href.split("/")[1] === item?.url?.split("/")[1]) ||
+    (mainNav &&
+      href.split("/")[1] !== "" &&
+      !!item?.items?.filter((i) => i.url.split("/")[1] === href.split("/")[1]).length)
+  );
+}
 
 const RadarSidebar = () => {
   const location = useLocation();
@@ -36,7 +50,7 @@ const RadarSidebar = () => {
                 <Collapsible key={item.title} asChild className="group/collapsible">
                   <SidebarMenuItem>
                     <CollapsibleTrigger asChild>
-                      <SidebarMenuButton tooltip={item.title} isActive={location.pathname === item.url}>
+                      <SidebarMenuButton tooltip={item.title} isActive={checkIsActive(location.pathname, item, true)}>
                         {item.icon && <item.icon />}
                         <span>{item.title}</span>
                         <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
@@ -46,7 +60,7 @@ const RadarSidebar = () => {
                       <SidebarMenuSub>
                         {item.items?.map((subItem) => (
                           <SidebarMenuSubItem key={subItem.title}>
-                            <SidebarMenuSubButton asChild isActive={location.pathname === subItem.url}>
+                            <SidebarMenuSubButton asChild isActive={checkIsActive(location.pathname, subItem)}>
                               <Link to={subItem.url}>
                                 <span>{subItem.title}</span>
                               </Link>
