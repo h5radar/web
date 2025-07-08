fc791c46da743fd10c573d13c571c605a09faf5b
ren3459 <55650591+ren3459@users.noreply.github.com>
2025-07-08 12:00:25 +0300

    The props for placeholder to filter-input has been added (#201)
    The props for placeholder to filter-input has been added (#201)
    ---------
    
    Co-authored-by: arufanov <arufanov@gmail.com>

--- src/pages/license/table.tsx
+++ src/pages/license/table.tsx
@@ -61,6 +61,7 @@ export const LicensesPage = () => {
         isLoading={isLoading}
         columns={columns}
         pageLink={"licenses"}
+        filterPlaceholder="Filter, for example: MIT%..."
         data={licenses.content}
         rowCount={licenses.totalElements}
         pageSize={licenses.pageable.pageSize}
