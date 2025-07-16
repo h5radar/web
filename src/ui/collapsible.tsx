9120081873201ea6f521e645952a36b3f12819d9
arufanov <arufanov@gmail.com>
2025-07-16 15:00:45 +0300

    The changes at collapsible have been removed

--- src/ui/collapsible.tsx
+++ src/ui/collapsible.tsx
@@ -1,8 +1,8 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
 function Collapsible({
   ...props
 }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
-
   return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
 }
 
