68ccfa131c75ce51560366265c89fac90a2e208b
ren3459 <ren3459@gmail.com>
2025-07-18 00:30:55 +0300

    add seedCompliances

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
 
+import { useSeedCompliances } from "@/queries/compliance";
 import { useSeedLicenses } from "@/queries/license";
 import { useSeedPractices } from "@/queries/practice";
 import { useSeedTechnologies } from "@/queries/technology";
@@ -13,14 +14,16 @@ export default function HomePage() {
   const { mutate: seedLicenses, isPending: isPending1 } = useSeedLicenses(auth, queryClient);
   const { mutate: seedPractices, isPending: isPending2 } = useSeedPractices(auth, queryClient);
   const { mutate: seedTechnologies, isPending: isPending3 } = useSeedTechnologies(auth, queryClient);
+  const { mutate: seedCompliances, isPending: isPending5 } = useSeedCompliances(auth, queryClient);
 
   useEffect(() => {
     seedLicenses();
     seedPractices();
     seedTechnologies();
-  }, [auth, seedLicenses, seedPractices, seedTechnologies]);
+    seedCompliances();
+  }, [auth, seedLicenses, seedPractices, seedTechnologies, seedCompliances]);
 
-  if (isPending1 || isPending2 || isPending3) {
+  if (isPending1 || isPending2 || isPending3 || isPending5) {
     return <h1>Loading...</h1>;
   }
 
