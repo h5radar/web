19fb133ad44500600295ad4cff87a65fb5caf54c
arufanov <arufanov@gmail.com>
2025-07-18 14:13:16 +0300

    The compliances should be seeded before license

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,19 +11,19 @@ export default function HomePage() {
   const auth = useAuth();
   const queryClient = useQueryClient();
 
-  const { mutate: seedLicenses, isPending: isPending1 } = useSeedLicenses(auth, queryClient);
-  const { mutate: seedPractices, isPending: isPending2 } = useSeedPractices(auth, queryClient);
-  const { mutate: seedTechnologies, isPending: isPending3 } = useSeedTechnologies(auth, queryClient);
-  const { mutate: seedCompliances, isPending: isPending5 } = useSeedCompliances(auth, queryClient);
+  const { mutate: seedCompliances, isPending: isPending1 } = useSeedCompliances(auth, queryClient);
+  const { mutate: seedLicenses, isPending: isPending2 } = useSeedLicenses(auth, queryClient);
+  const { mutate: seedPractices, isPending: isPending3 } = useSeedPractices(auth, queryClient);
+  const { mutate: seedTechnologies, isPending: isPending4 } = useSeedTechnologies(auth, queryClient);
 
   useEffect(() => {
+    seedCompliances();
     seedLicenses();
     seedPractices();
     seedTechnologies();
-    seedCompliances();
-  }, [auth, seedLicenses, seedPractices, seedTechnologies, seedCompliances]);
+  }, [auth, seedCompliances, seedLicenses, seedPractices, seedTechnologies]);
 
-  if (isPending1 || isPending2 || isPending3 || isPending5) {
+  if (isPending1 || isPending2 || isPending3 || isPending4) {
     return <h1>Loading...</h1>;
   }
 
