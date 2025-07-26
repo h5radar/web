68b42926976b563582074bcc86cc6a4125cef2e0
arufanov <arufanov@gmail.com>
2025-07-26 13:59:45 +0300

    The menu items have been cleaned

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,16 +1,10 @@
 import {
-  Activity,
   Barcode,
   Copyright,
-  Cpu,
-  Handshake,
   LayoutDashboard,
-  LogIn,
   LucideShoppingBag,
   Ruler,
   Settings,
-  UserPen,
-  Wallet,
 } from "lucide-react";
 
 import { NavItem } from "@/types/nav-item";
@@ -121,129 +115,5 @@ export const radarNavItems: NavItem[] = [
         items: [],
       },
     ],
-  },
-  {
-    title: "Contributions",
-    url: "#",
-    icon: Activity,
-    items: [
-      {
-        title: "Git repos",
-        url: "/repositories",
-        icon: UserPen,
-      },
-      {
-        title: "Platforms",
-        url: "/platforms",
-        icon: LogIn,
-      },
-      {
-        title: "Teams",
-        url: "/teams",
-        icon: LogIn,
-      },
-      {
-        title: "Authors",
-        url: "/authors",
-        icon: LogIn,
-      },
-    ],
-  },
-  {
-    title: "Technologies",
-    url: "#",
-    icon: Cpu,
-    items: [
-      {
-        title: "Radars",
-        url: "/radars",
-        icon: LucideShoppingBag,
-        items: [],
-      },
-      {
-        title: "Rings",
-        url: "/rings",
-        icon: LucideShoppingBag,
-        items: [],
-      },
-      {
-        title: "Segments",
-        url: "/segments",
-        icon: LucideShoppingBag,
-        items: [],
-      },
-      {
-        title: "Technology blips",
-        url: "/technology-blips",
-        icon: LucideShoppingBag,
-        items: [],
-      },
-    ],
-  },
-  {
-    title: "Practices",
-    url: "#",
-    icon: Ruler,
-    items: [
-      {
-        title: "Cobwebs",
-        url: "/cobwebs",
-        icon: Ruler,
-        items: [],
-      },
-      {
-        title: "Rays",
-        url: "/rays",
-        icon: Ruler,
-        items: [],
-      },
-    ],
-  },
-  {
-    title: "Licenses",
-    url: "#",
-    icon: Copyright,
-    items: [
-      {
-        title: "Pie charts",
-        url: "/pies",
-        icon: Copyright,
-        items: [],
-      },
-    ],
-  },
-  {
-    title: "Communications",
-    url: "#",
-    icon: Handshake,
-    items: [
-      {
-        title: "Channels",
-        url: "/channels",
-        icon: Handshake,
-      },
-      {
-        title: "Participants",
-        url: "/participants",
-        icon: Handshake,
-      },
-    ],
-  },
-  {
-    title: "Finances",
-    url: "#",
-    icon: Wallet,
-    items: [
-      {
-        title: "BI systems",
-        url: "/bi-systems",
-        icon: Wallet,
-      },
-    ],
-  },
-  {
-    title: "Adapters",
-    url: "/adapters",
-    icon: Settings,
   },
 ];
