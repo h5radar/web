703fc0203b95b082726228585d62f2908ed0bccc
ren3459 <55650591+ren3459@users.noreply.github.com>
2025-07-27 10:14:11 +0300

    remove duplicated tracker

--- src/layouts/account.tsx
+++ src/layouts/account.tsx
@@ -9,15 +9,12 @@ import { accountSearchItem } from "@/constants/search";
 
 import { SearchProvider } from "@/providers/search";
 
-import usePageTracking from "@/hooks/use-page-tracking";
-
 import AccountSidebar from "@/components/account-sidebar";
 import AppNavbar from "@/components/app-navbar";
 
 export default function AccountLayout() {
   // Persisting the sidebar state in the cookie.
   const defaultOpen = Cookies.get("sidebar_state") !== "false";
-  usePageTracking();
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true} storageKey="dashboard-theme">
