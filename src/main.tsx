64788b0ca5325cece20a6582155dbef87793da90
arufanov <arufanov@gmail.com>
2025-08-12 16:43:55 +0300

    The brakets have been added for import.meta.env.VITE_GOOGLE_ANALYTICS

--- src/main.tsx
+++ src/main.tsx
@@ -95,7 +95,11 @@ const queryClient = new QueryClient({
     },
   }),
 });
-if (import.meta.env.VITE_GOOGLE_ANALYTICS) ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS);
+
+if (import.meta.env.VITE_GOOGLE_ANALYTICS) {
+  ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
