b4d01d7123bcbbb14b377ee0068322881dba0217
arufanov <arufanov@gmail.com>
2025-09-05 09:39:35 +0300

    The syntax sugar refactoring

---
+++ src/constants/query-keys.ts
@@ -0,0 +1,72 @@
+/*
+ * TanStack query keys for auth
+ */
+export const GET_AUTH_HEALTH = "Get auth health";
+
+/*
+ * TanStack query keys for licenses
+ */
+export const CREATE_LICENSE = "Create license";
+export const UPDATE_LICENSE = "Update license";
+export const DELETE_LICENSE = "Delete license";
+export const GET_LICENSE = "Get license";
+export const GET_LICENSES = "Get licenses";
+export const SEED_LICENSES = "Seed licenses";
+
+/*
+ * TanStack query keys for practices
+ */
+export const CREATE_PRACTICE = "Create practice";
+export const UPDATE_PRACTICE = "Update practice";
+export const DELETE_PRACTICE = "Delete practice";
+export const GET_PRACTICE = "Get practice";
+export const GET_PRACTICES = "Get practices";
+export const SEED_PRACTICES = "Seed practices";
+
+/*
+ * TanStack query keys for technology
+ */
+export const CREATE_TECHNOLOGY = "Create technology";
+export const UPDATE_TECHNOLOGY = "Update technology";
+export const DELETE_TECHNOLOGY = "Delete technology";
+export const GET_TECHNOLOGY = "Get technology";
+export const GET_TECHNOLOGIES = "Get technologies";
+export const SEED_TECHNOLOGIES = "Seed technologies";
+
+/*
+ * TanStack query keys for products
+ */
+export const CREATE_PRODUCT = "Create product";
+export const UPDATE_PRODUCT = "Update product";
+export const DELETE_PRODUCT = "Delete product";
+export const GET_PRODUCT = "Get product";
+export const GET_PRODUCTS = "Get products";
+export const SEED_PRODUCTS = "Seed products";
+
+/*
+ * TanStack query keys for maturity
+ */
+export const CREATE_MATURITY = "Create maturity";
+export const UPDATE_MATURITY = "Update maturity";
+export const DELETE_MATURITY = "Delete maturity";
+export const GET_MATURITY = "Get maturity";
+export const GET_MATURITIES = "Get maturities";
+export const SEED_MATURITIES = "Seed maturities";
+/*
+ * TanStack query keys for compliances
+ */
+export const CREATE_COMPLIANCE = "Create compliance";
+export const UPDATE_COMPLIANCE = "Update compliance";
+export const DELETE_COMPLIANCE = "Delete compliance";
+export const GET_COMPLIANCE = "Get compliance";
+export const GET_COMPLIANCES = "Get compliances";
+export const SEED_COMPLIANCES = "Seed compliances";
+/*
+ * TanStack query keys for domains
+ */
+export const CREATE_DOMAIN = "Create domain";
+export const UPDATE_DOMAIN = "Update domain";
+export const DELETE_DOMAIN = "Delete domain";
+export const GET_DOMAIN = "Get domain";
+export const GET_DOMAINS = "Get domains";
+export const SEED_DOMAINS = "Seed domains";
