e4dcc082b2d16e7eab5c89320b6908bb8d4840fd
ren3459 <ren3459@gmail.com>
2025-09-08 23:59:55 +0300

    add text

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -24,13 +24,20 @@ export default function WelcomePage() {
             </CardContent>
           </Card>
           {toBoolean(import.meta.env.VITE_ENABLED_DEMO_BANNER) && (
-            <Card className="bg-neutral-800">
-              <CardHeader className="text-center">
-                <CardTitle className="text-2xl">Welcome to H5Radar Demo</CardTitle>
+            <Card className="bg-neutral-800 text-center">
+              <CardContent>
+                <CardHeader className="text-center">
+                  <CardTitle className="text-2xl">Welcome to H5Radar Demo</CardTitle>
+                </CardHeader>
+                <p>Available demo scenarios</p>
+                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                  <li>License overview by compliance – see how licenses are grouped and analyzed.</li>
+                  <li>Technologies view – explore technologies in both table format and radar visualization.</li>
+                </ul>
                 <CardDescription>
                   You can explore the application with the demo account alice@example / secret.
                 </CardDescription>
-              </CardHeader>
+              </CardContent>
             </Card>
           )}
         </div>
